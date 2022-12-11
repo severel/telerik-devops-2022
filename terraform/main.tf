2f4c1ada41fc2206144d857767cb0ec8a9bed2be
Daniel Rankov <daniel@severalclouds.com>
2022-12-11 18:16:21 +0200

    provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-01cae1550c0adea9c"
   instance_type = "t2.micro"
